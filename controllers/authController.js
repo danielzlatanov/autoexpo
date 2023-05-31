487c5bb912a46f38edcbf76db082099728197036
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-05-31 18:48:54 +0300

    added user model and implemented register to DB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,33 @@
 const router = require('express').Router();
-const { login } = require('../services/authService.js');
+const { login, register } = require('../services/authService.js');
 
 router.get('/login', (req, res) => {
   res.render('login', {
-    title: 'Sign In'
+    title: 'Sign In',
   });
 });
 
 router.post('/login', async (req, res) => {
   const result = await login(req.body.username, req.body.password);
-  const token = req.signJwt(result);
-  res.cookie('jwt', token);
+  attachJwt(req,res,result);
   res.redirect('/');
 });
+
+router.get('/register', (req, res) => {
+  res.render('register', {
+    title: 'Sign Up',
+  });
+});
+
+router.post('/register', async (req, res) => {
+  const result = await register(req.body.username, req.body.password);
+  attachJwt(req,res,result);
+  res.redirect('/');
+});
+
+function attachJwt(req, res, data) {
+  const token = req.signJwt(data);
+  res.cookie('jwt', token, { maxAge: 14400000 });
+}
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,3 +1,26 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User.js');
+
+async function register(username, password) {
+  const existing = await User.findOne({
+    username: { $regex: new RegExp(username), $options: 'i' },
+  });
+  if (existing) {
+    throw new Error('username is taken');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    hashedPassword,
+  });
+
+  return {
+    username,
+    roles: user.roles,
+  };
+}
+
 async function login(username, password) {
   return new Promise((resolve, reject) => {
     if (username.toLowerCase() === 'daniel' && password === '123') {
@@ -7,11 +30,12 @@ async function login(username, password) {
         roles: ['user'],
       });
     } else {
-      reject(new Error('Invalid username or password'));
+      reject(new Error('invalid username or password'));
     }
   });
 }
 
 module.exports = {
+  register,
   login,
 };
